40050ede62e018366e7871a4f28c5cd7bc9af59e
Mike Helmick <mike.helmick@gmail.com>
2013-03-08 13:05:35 -0700

    added link to github repo

--- parts/bottom.php
+++ parts/bottom.php
@@ -1,6 +1,7 @@
 <hr>
   <div class="footer">
     <p>&copy; SIGCSE 2013</p>
+    <p>Follow the source at <a href="https://github.com/sigcsets/sigcse2014">https://github.com/sigcsets/sigcse2014</a></p>
   </div>
 
 </div> <!-- /container -->
