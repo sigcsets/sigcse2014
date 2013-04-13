be204fa457ed3f391e33aefdb2924a4fd202882e
Mike Helmick <mike.helmick@gmail.com>
2013-04-12 22:22:36 -0700

    add analytics for this site

--- parts/bottom.php
+++ parts/bottom.php
@@ -10,5 +10,16 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="<?php if ($local) echo "/sigcse2014" ?>/js/jquery.js"></script>
 <script src="<?php if ($local) echo "/sigcse2014" ?>/js/bootstrap.min.js"></script>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40114524-1', 'sigcse.org');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
