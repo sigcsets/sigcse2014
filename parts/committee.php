c2a26596e6afdd0af355a4a1beab2bd52d6ad801
Kimberly Voll <zanytomato@gmail.com>
2013-07-29 22:09:13 -0700

    Updated committee page

--- parts/committee.php
+++ parts/committee.php
@@ -1,139 +1,246 @@
 <div class="row-fluid" id="committee">
   <div class="span4">
     <p><strong>Symposium Chairs</strong><br/>
+    <script>write_email("sigcse2014-symposium", "ggc.edu");</script></p>
+
     J.D. Dougherty<br/>
     Haverford College<br/>
+    
+    <img src="../img/CommitteeMembers/JDDougherty.png" alt="JD Dougherty" width="128"  />
+    &nbsp;</br>
     &nbsp;</br>
     Kris Nagel<br/>
     Georgia Gwinnett College<br/>
-    <script>write_email("sigcse2014-symposium", "ggc.edu");</script></p>
+    <img src="../img/CommitteeMembers/KrisNagel.jpg" alt="Kris Nagel" width="128"  />
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Program Chairs</strong><br/>
+    <script>write_email("sigcse2014-program", "ggc.edu");</script></p>
     Adrienne Decker<br/>
     Rochester Institute of Technology<br/>
+    <img src="../img/CommitteeMembers/AdrienneDecker.jpg" alt="Adrienne Decker" width="128"/>
+    &nbsp;</br>
     &nbsp;<br/>
+
     Kurt Eiselt<br/>
     University of British Columbia<br/>
-    <script>write_email("sigcse2014-program", "ggc.edu");</script></p>
+    <img src="../img/sigcse_logo_small.png" alt="Kurt Eiselt"  width="128" />
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Panels and Special Sessions</strong><br/>
+    <script>write_email("sigcse2014-panels", "ggc.edu");</script></p>
     Stephen Cooper<br/>
     Stanford University<br/>
-    <script>write_email("sigcse2014-panels", "ggc.edu");</script></p>
+    <img src="../img/sigcse_logo_small.png" alt="Stephen Cooper" width="128" />
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Workshops</strong><br/>
+    <script>write_email("sigcse2014-workshops", "ggc.edu");</script></p>
     Susan Haller<br/>
     SUNY Potsdam<br/>
-    &nbsp;</br/>
+    <img src="../img/CommitteeMembers/SusanHaller.gif" alt="Susan Haller" width="128" />
+    &nbsp;</br>
+    &nbsp;</br>
+
     Sue Fitzgerald<br/>
     Metropolitan State University <br/>
-    <script>write_email("sigcse2014-workshops", "ggc.edu");</script></p>
+    <img src="../img/CommitteeMembers/SueFitzgerald.gif" alt="Sue Fitzgerald" width="128" />
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Publications</strong><br/>
+    <script>write_email("sigcse2014-publications", "ggc.edu");</script></p>
     Dennis J. Bouvier<br/>
     Southern Illinois University Edwardsville<br/>
-    <script>write_email("sigcse2014-publications", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Dennis J. Bouvier" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Database Administrators</strong><br/>
+    <script>write_email("sigcse2014-database", "ggc.edu");</script></p>
     John Dooley<br/>
     Knox College<br/>
-    &nbsp;<br/>
+        <img src="../img/CommitteeMembers/JohnDooley.jpg" alt="John Dooley" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
+    
     Henry Walker<br/>
     Grinnell College<br/>
-    <script>write_email("sigcse2014-database", "ggc.edu");</script></p>
+        <img src="../img/CommitteeMembers/HenryWalker.jpg" alt="Henry Walker" width="128"/>
+    &nbsp;</br>
+
   </div>
 
   <div class="span4">
     <p><strong>Registration</strong><br/>
+    <script>write_email("sigcse2014-registration", "ggc.edu");</script></p>
     Lynn Degler<br/>
     Rose-Hulman Institute of Technology<br/>
+    <img src="../img/CommitteeMembers/LynnDegler.jpg" alt="Lynn Degler" width="128" />
+    &nbsp;</br>
     &nbsp;</br>
     Cary Laxer<br/>
     Rose-Hulman Institute of Technology<br/>
+    <img src="../img/CommitteeMembers/CaryLaxer.jpg" alt="Cary Laxer" width="128" />
+    &nbsp;</br>
     &nbsp;</br>
     Larry Merkle<br/>
     Computational Optimization Services<br/>
-    <script>write_email("sigcse2014-registration", "ggc.edu");</script></p>
+        <img src="../img/CommitteeMembers/LarryMerkle.jpg" alt="Larry Merkle" width="128" />
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Posters</strong><br/>
+    <script>write_email("sigcse2014-posters", "ggc.edu");</script></p>
     Michael T. Helmick<br/>
     University of Cincinnati<br/>
-    <script>write_email("sigcse2014-posters", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Michael T. Helmick" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>    
 
     <p><strong>Birds Of A Feather</strong><br/>
+    <script>write_email("sigcse2014-bofs", "ggc.edu");</script></p>
     Marc Rubin<br/>
     Colorado School of Mines<br/>
-    <script>write_email("sigcse2014-bofs", "ggc.edu");</script></p>
+        <img src="../img/CommitteeMembers/MarcRubin.jpg" alt="Marc Rubin" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Student Volunteers and Student Activities</strong><br/>
+    <script>write_email("sigcse2014-volunteers", "ggc.edu");</script></p>
     Steven Wolfman<br/>
     University of British Columbia<br/>
-    &nbsp;<br/>
+        <img src="../img/CommitteeMembers/SteveWolfman.jpg" alt="Steven Wolfman" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
     Pam Cutter<br/>
     Kalamazoo College<br/>
-    <script>write_email("sigcse2014-volunteers", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Pam Cutter" width="128"/>
+    &nbsp;</br>
+    
 
     <p><strong>Webmasters</strong><br/>
+    <script>write_email("sigcse2014-webmaster", "ggc.edu");</script></p>
     Kimberly Voll<br/>
     Centre for Digital Media<br/>
-    &nbsp;
+    <img src="../img/CommitteeMembers/KimberlyVoll.jpg" alt="Kimberly Voll" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
     Michael T. Helmick<br/>
     University of Cincinnati<br/>
-    <script>write_email("sigcse2014-webmaster", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Michael T. Helmick" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Publicity/Social Media</strong><br/>
+    <script>write_email("sigcse2014-publicity", "ggc.edu");</script></p>
     Jamie Spacco<br/>
     Knox College<br/>
-    <script>write_email("sigcse2014-publicity", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Jamie Spacco" width="128"/>
+    &nbsp;</br>
+    
 
     <p><strong>Treasurer</strong><br/>
+    <script>write_email("sigcse2014-treasurer", "ggc.edu");</script></p>
     Scott McElfresh<br/>
     Longwood University<br/>
-    <script>write_email("sigcse2014-treasurer", "ggc.edu");</script></p>
-  </div>
+        <img src="../img/CommitteeMembers/ScottMcElfresh.jpg" alt="Scott McElfresh" width="128" />
+    &nbsp;</br>
+    
+</div>
 
-  <div class="span4">
+<div class="span4">
     <p><strong>Evaluations</strong><br/>
+    <script>write_email("sigcse2014-evaluations", "ggc.edu");</script></p>
     Carl Alphonce<br/>
     University at Buffalo<br/>
-    <script>write_email("sigcse2014-evaluations", "ggc.edu");</script></p>
-   
+        <img src="../img/sigcse_logo_small.png" alt="Carl Alphonce" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>   
+
     <p><strong>Kids’ Camp</strong><br/>
+    <script>write_email("sigcse2014-kidscamp", "ggc.edu");</script></p>
     James Maher<br/>
     Colorado School of Mines<br/>
-    &nbsp;<br/>
+        <img src="../img/CommitteeMembers/JamesMaher.jpg" alt="James Maher" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
     Cheryl Seals<br/>
     Auburn University<br/>
-    <script>write_email("sigcse2014-kidscamp", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Cheryl Seals" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Support/Exhibitor Liaison</strong><br/>
+    <script>write_email("sigcse2014-exhibitors", "ggc.edu");</script></p>
     Susan Rodger<br/>
     Duke University<br/>
-    <script>write_email("sigcse2014-exhibitors", "ggc.edu");</script></p>
+        <img src="../img/CommitteeMembers/SusanRodger.png" alt="Susan Rodger" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Pre-Conference Events Liaison</strong><br/>
+    <script>write_email("sigcse2014-preconference", "ggc.edu");</script></p>
     Mary Anne Egan<br/>
     Siena College<br/>
-    <script>write_email("sigcse2014-preconference", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Mary Anne" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>K-12 Liaison</strong><br/>
+    <script>write_email("sigcse2014-k12", "ggc.edu");</script></p>
     Crystal Furman<br/>
     Brookwood High School<br/>
-    <script>write_email("sigcse2014-k12", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Crystal Furman" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
-    <p><strong>International Liaison</strong>
+    <p><strong><a href="#international">International Liaison</a></strong><br/>
+    <script>write_email("sigcse2014-international", "ggc.edu");</script></p>
     Annemieke Craig<br/>
     Deakin University<br/>
-    <script>write_email("sigcse2014-international", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Annemieke Craig" width="128" />
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Local Arrangements</strong><br/>
+    <script>write_email("sigcse2014-local", "ggc.edu");</script></p>
     Deepa Muralidhar<br/>
     North Gwinnett High School<br/>
-    <script>write_email("sigcse2014-local", "ggc.edu");</script></p>
+        <img src="../img/sigcse_logo_small.png" alt="Deepa Muralidhar" width="128"/>
+    &nbsp;</br>
+    &nbsp;</br>
 
     <p><strong>Student Research Competition</strong><br/>
+    <script>write_email("sigcse2014-src", "ggc.edu");</script></p>
     Ann Sobel<br/>
     Miami University (Ohio)<br/>
-    <script>write_email("sigcse2014-src", "ggc.edu");</script></p>
+        <img src="../img/CommitteeMembers/AnnSobel.jpg" alt="Ann Sobel" width="128"/>
+    &nbsp;</br>
   </div>
 </div>
+
+<hr/>
+
+ <div class="section" style="text-align: center;" id="international">
+   <h1>International Committee</h1>
+ </div>
+
+<ul>
+<li><strong>Annemieke Craig (Chair)</strong>    Deakin University, Australia    <script>write_email("acraig", "deakin.edu.au");</script></li>
+<li><strong>Reyyan Ayfer</strong>    Bilkent University, Turkey  <script>write_email("ayfer","bilkent.edu.tr");</script></li>
+<li><strong>Alison Clear</strong>    Christchurch Polytechnic Institute of Technology, New Zealand   <script>write_email("Alison.Clear","cpit.ac.nz");</script></li>
+<li><strong>Claudia Bauzer Madeiros</strong> UNICAMP, Brazil <script>write_email("Brazil cmbm","ic.unicamp.br");</script></li>
+<li><strong>Yasuto Shirai</strong>   Shizuoka University, Japan  <script>write_email("shirai","inf.shizuoka.ac.jp");</script></li>
+<li><strong>Sridhar Iyer</strong>    Indian Institute of Technology, India   <script>write_email("sri","iitb.ac.in");</script></li>
+<li><strong>Gary K. W. Wong</strong> The Hong Kong Institute of Education    <script>write_email("wongkawai","ied.edu.hk");</script></li>
+<li><strong>Carsten Kleiner</strong>     Hanover, Germany    <script>write_email("carsten.kleiner","hs-hannover.de");</script></li>
+<li><strong>Simon Humphreys</strong> Cambridge, UK   <script>write_email("simon.humphreys","computingatschool.org.uk");</script></li>
+<li><strong>Prof. Judith Gal-Ezer</strong>   The Open University of Israel   <script>write_email("galezer","openu.ac.il");</script></li>
+<li><strong>Dr Karen Bradshaw</strong>  Rhodes University, South Africa <script>write_email("K.Bradshaw","ru.ac.za");</script></li>
+<li><strong>Ming Zhang</strong>  Peking University, China    <script>write_email("mzhang","net.pku.edu.cn");</script></li>
+<li><strong>Michail Giannakos</strong>   Norwegian University of Science and Technology  <script>write_email("michail.giannakos","idi.ntnu.no");</script></li>
+</ul>
