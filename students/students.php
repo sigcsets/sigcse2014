89e33692528efd75280c267d3182b5e865490e8b
Kimberly Voll <zanytomato@gmail.com>
2013-11-18 10:40:21 -0800

    making sure repo is up-to-date with web chanegs

---
+++ students/students.php
@@ -0,0 +1,64 @@
+<?php
+$title = "SIGCSE 2014 - Atlanta, GA";
+$nav_menu = "students";
+include("parts/top.php");
+?>
+
+<li><a href="#register"><i class="icon-chevron-right"></i> Register</a></li>
+<li><a href="#supporters"><i class="icon-chevron-right"></i> Supporters</a></li>
+<li><a href="#learningatscale"><i class="icon-chevron-right"></i> Learning @ Scale</a></li>
+
+<?php 
+include("parts/middle.php"); 
+?>
+
+<noscript>
+  <div class="alert alert-error"><i class="icon-exclamation-sign"></i> Javascript is required to navigate the SIGCSE 2014 Web site. Please enable Javascript in your browser.</div> 
+</noscript>
+
+<div class="row-fluid">
+  <div class="span8">
+    <img src="<?php echo $prefix ?>/img/sigcse_logo_large.png" width="360" style="display:block;margin-left:auto; margin-right:auto;"/>
+
+    <div style="text-align: center;" id="welcome"><h2>Student Volunteers</h2></div>
+
+
+Student Volunteers @ SIGCSE 2013
+Student volunteers are an important part of the SIGCSE Symposium and have been for many years. Each volunteer works for up to 6 hours, helping with various conference management activities. You'll interact with the conference organizers and presenters and will meet other students from all over the world. More information about the experience, the benefits, and how to sign up are provided below.
+
+How do I register to volunteer?
+Register for SIGCSE as a student. (Your registration fee will be refunded after you complete your volunteer hours.)
+SEPARATELY register as a volunteer with the student volunteering team.
+In previous years we have been able to accept everyone who registered in December and January and often anyone interested. While this can vary by year, we try hard to accommodate as many students as possible. Having said this, it helps us and you to register as early as you can. You can do the initial registration and indicate work time later (we understand that it can take a little while to arrange travel). You will be notified if you need to complete travel plans to remain on the volunteer list. We also greatly appreciate you going back to the registration system to cancel if that becomes necessary. Note you will need to pay the registration fee and the costs of any additions to registration, but the registration fee itself will be refunded once you complete your volunteer hours.
+
+Upon arrival at the conference site, you must check in at the Registration Desk to receive your conference registration packet then visit the Student Volunteer Office to check in (so we know you are around) and to receive any needed instructions.
+
+Registration Refund: If the registration is paid in cash, then your refund will be paid immedately in cash. If the registration is paid by credit card, then your refund will be processed anywhere from one day to one week after the conference ends. If the registration is paid by check, then your refund will be by check through ACM which may take up to one month after the conference is over. This assumes you successfully complete your volunteer duties.
+
+Who is eligible to be a student volunteer?
+Any full-time student may become a student volunteer. Both undergraduate and graduate students are welcome. You specify your school affiliation when you sign up as a volunteer, and you should bring a copy of your current course schedule to the conference as verification of your full time status. Students attending the conference for the Research Poster competition are encouraged to become volunteers as well.
+
+Important information for students requiring a visa: The Association for Computing Machinery (ACM, SIGCSE's umbrella organization) does not issue formal invitation letters for visas. However, they can issue a visa support letter. For a visa support letter, please send a request to Laura Bucci (Bucci@hq.acm.org)with the following information:
+
+What do I get in return for being a student volunteer?
+In exchange for 5 hours of conference service (6 hour maximum), you will receive:
+
+Note that in addition to these benefits, since you are a registered SIGCSE student attendee, you have all the rights of the non-volunteer student attendees:
+
+What kinds of work will I do as a student volunteer?
+It varies. You might work at the registration booth to help organize the packets that we give out to attendees. You might be at the door of the exhibits hall to verify that the people entering have a conference badge. You might count the participants at various sessions. You might hang out at certain sessions to assist the speakers with handouts or equipment.
+
+Do I have a say in what work I do as a student volunteer?
+Not much, but we'll do what we can. If you have a skill we are interested in then you get a chance to indicate that when you are indicating available work times. We also try to help students swap sessions if you have one you particularly want to attend but cannot do that until you come to the conference and we cannot make guarantees.
+
+When during the conference will I do my student volunteer work?
+This issue is driven by conference needs. Some time periods are busier than others. We'll take your preferences into account whenever possible. When you sign up as a student volunteer, you'll specify when you plan to arrive and leave the conference. You will also have the opportunity to specify when you are presenting a paper or poster. Within those constraints, we'll make some initial plans. We realize that things may come up and will try to make dynamic changes as needed. In return, we ask that you be as flexible as possible. Your duties may be broken into separate time periods and even across separate days. We guarantee no more than 6 hours of assigned work per volunteer but target an average of 5 hours.
+
+Do I have to be there for the whole conference to be a student volunteer?
+No. We realize that travel plans and other commitments exist. We hope that students local to the conference area will be flexible. In particular, there is always a need for volunteers on Tuesday and Wednesday, which is before most of the conference activities begin and before many of the traveling student volunteers have arrived. We encourage local student volunteers to be available on Tuesday and Wednesday.
+
+Can I get funding for travel or hotel expenses?
+The conference itself does not provide funding for student volunteers, but you may be able to get some support through your department or through another source at your university. You should print your registration acknowledgment to show to potential funding sources.
+
+Questions
+If you have questions about paper submissions send e-mail to sigcse13-volunteers@cs.rit.edu (Steven Huss-Lederman and Steve Wolfman).
---
+++ students/volunteers.php
@@ -0,0 +1,136 @@
+<?php
+$title = "SIGCSE 2014 - Atlanta, GA";
+$nav_menu = "students";
+include("../parts/top.php");
+?>
+
+<li><a href="#register"><i class="icon-chevron-right"></i> Register</a></li>
+<li><a href="#attheconference"><i class="icon-chevron-right"></i> At the Conference</a></li>
+<li><a href="#faq"><i class="icon-chevron-right"></i> FAQ</a></li>
+<li><a href="#questions"><i class="icon-chevron-right"></i> Questions?</a></li>
+
+<?php 
+include("../parts/middle.php"); 
+?>
+
+<noscript>
+  <div class="alert alert-error"><i class="icon-exclamation-sign"></i> Javascript is required to navigate the SIGCSE 2014 Web site. Please enable Javascript in your browser.</div> 
+</noscript>
+
+<div class="row-fluid">
+  <div class="span8">
+    <img src="<?php echo $prefix ?>/img/sigcse_logo_large.png" width="360" style="display:block;margin-left:auto; margin-right:auto;"/>
+
+    <div style="text-align: center;" id="welcome"><h2>Student Volunteers</h2></div>
+
+	<p>Student volunteers are an important part of the SIGCSE Symposium and have been for many years. Each volunteer works for up to 6 hours, helping 
+	with various conference management activities. You'll interact with the conference organizers and presenters and will meet other students from all 
+	over the world. More information about the experience, the benefits, and how to sign up are provided below.</p>
+
+    <div style="text-align: center;" id="register"><h2>How do I register to volunteer?</h2></div>
+	
+	<p>Register for SIGCSE as a student. Note you will need to pay the registration fee and the costs of any additions to registration, but 
+	the registration fee itself will be refunded once you complete your volunteer hours.</p>
+	<p><strong>SEPARATELY</strong> register as a volunteer with the student volunteering team.</p>
+	<p>Please note that while in previous years we have been able to accept everyone who registered in December and January and often anyone interested, 
+	we cannot guarantee that this is the case every year.  Please try to register as early as possible (you can provide your availability for shifts closer
+	to the conference start as we understand that it can take a little while to arrange travel).</p>
+	<p>Also, please note that if you are no longer able to attend, we greatly appreciate you going back to the 
+	registration system to cancel.</p>
+
+    <div style="text-align: center;" id="attheconference"><h2>How do I register to volunteer?</h2></div>
+
+	<p>Upon arrival at the conference site, you must check in at the <strong>Registration Desk</strong> to receive your conference registration packet. 
+	Then visit the <strong>Student Volunteer Office</strong> to check in (so we know you are around) and to receive any needed instructions or updates.</p>
+
+	<h3>Registration Refund</h3>
+	<p>Assuming you have successfully completed your volunteer duties as signed off by your volunteer supervisor, you are eligible to receive a registration
+	refund:</p>
+	<ul>
+	<li>If your registration was paid in cash,  your refund will be paid immedately in cash.</li>
+	<li>If your registration was paid by credit card, your refund will be processed anywhere from one day to one week after the conference ends.</li>
+	<li>If your registration was paid by cheque,  your refund will be a cheque issued via ACM, which may take up to one month after the conference is over.</li>
+	</ul>
+
+
+    <div style="text-align: center;" id="faq"><h2>Frequently Asked Questions</h2></div>
+
+	<h2>Who is eligible to be a student volunteer?</h2>
+	<p>Any full-time student may become a student volunteer. Both undergraduate and graduate students are welcome. You specify your school affiliation 
+	when you sign up as a volunteer, and you should bring a copy of your current course schedule to the conference as verification of your full time 
+	status. Students attending the conference for the Research Poster competition are encouraged to become volunteers as well.</p>
+
+	<h2>What if I require a visa?</h2>
+	<p>The Association for Computing Machinery (ACM, SIGCSE's umbrella organization) does not issue 
+	formal invitation letters for visas. However, they can issue a visa support letter. For a visa support letter, please send a request to 
+	Laura Bucci (Bucci@hq.acm.org) with the following information:</p>
+		<ul>
+			<li>Name and mailing address as it appears on your passport.</li>
+			<li>The name of the conference you wish to attend (SIGCSE 2014).</li>
+			<li>Registration confirmation number.</li>
+			<li>If you are the author of any papers accepted for the conference, please provide the title.</li>
+			<li>Fax number (visa support letters are not issued via email).</li>
+		</ul>
+
+	<h2>What are the benefits of student volunteering?</h2>
+
+	<p>In exchange for 5 hours of conference service (6 hour maximum), you will receive:
+	<ul>
+	<li>Refunded registration fee.</li>
+	<li>A chance to meet and work with students from other schools.</li>
+	<li>Entrance to the student volunteer party.</li>
+	<li>The opportunity to contribute to an effort to improve Computer Science education... and our sincere gratitude for your efforts!</li>
+	</ul>
+	<p>Note that in addition to these benefits, since you are a registered SIGCSE student attendee, you have all the rights of the non-volunteer 
+	student attendees:
+	<ul>
+	<li>Access to all sessions, including papers, panels, seminars, and birds-of-a-feather discussions.</li>
+	<li>Access to all exhibits where book publishers and software vendors have displays and activities.</li>
+	<li>All conference "coffee" breaks (some food and drink).</li>
+	<li>The main conference reception on Thursday evening (typically includes a light dinner).</li>
+	<li>The conference luncheon on Saturday afternoon.</li>
+	</ul>
+
+
+	<h2>What kinds of work will I do as a student volunteer?</h2>
+	<p>It varies.  Duties may include (and are not limited to) the following:</p>
+	<ul>
+	<li>You may work at the registration booth organizing attendee packets.</li>
+	<li>You may monitor the exhibit hall entrance to ensure those entering have the appropriate conference badge.</li> 
+	<li>You may count the attendees at various conference sessions.</li> 
+	<li>You may hang out at certain sessions to assist the speakers with handouts or equipment.</li>
+	</ul>
+
+	<h2>Do I have a say in what work I do as a student volunteer?</h2>
+	<p>Not much, but we'll do what we can. If you have a skill we are interested in then you get a chance to indicate that when 
+	you are indicating available work times (e.g. like working children might make you a great choice to help with kids camp!). 
+	We also try to accommodate students who have a preference for a particular shift or session, but due to the complexity of scheduling
+	and ensuring we have adequate coverage at all sessions, we cannot guarantee anything.</p>
+
+	<h2>When during the conference will I do my student volunteer work?</h2>
+	<p>This will depend on conference needs; some time periods are busier than others. Your duties may be broken into 
+	separate time periods and even across separate days. We guarantee no more than 6 hours of assigned work per volunteer but target an average 
+	of 5 hours.</p>
+	<p>Please note that we'll take your preferences into account whenever possible, as 
+	well as the times you specified that you will be arriving at and leaving the conference. You will also have the opportunity 
+	to specify when you are presenting a paper or poster. Given those constraints we will make our schedule.</p>
+	<p>We recognize that things may change and will do our best make changes as needed. In return, we ask that you be as flexible as possible as 
+	coordinating a conference is challenging and your role is important!</p>
+	
+
+	<h2>Do I have to be there for the whole conference to be a student volunteer?</h2>
+    <p>No. We realize that travel plans and other commitments exist. We hope that students local to the conference area will be flexible. 
+    In particular, there is always a need for volunteers on Tuesday and Wednesday, before most of the conference activities begin 
+    and before many of the traveling student volunteers have arrived. We encourage local student volunteers to be available on Tuesday and 
+    Wednesday.</p>
+
+	<h2>Can I get funding for travel or hotel expenses?</h2>
+	<p>The conference itself does not provide funding for student volunteers, but you may be able to get some support through your department 
+	or through another source at your university. You should print your registration acknowledgment to show to potential funding sources.</p>
+
+    <div style="text-align: center;" id="questions"><h2>More Questions</h2></div>
+
+	<p>If you have questions about volunteering, please send an e-mail to sigcse13-volunteers@cs.rit.edu</p>
+
+
+
