814a2990239fa54796d34c8404aa42e9fd8a3588
Kimberly Voll <zanytomato@gmail.com>
2013-12-06 09:23:09 -0800

    catching up to a rash of changes

---
+++ attendees/keynote.php
@@ -0,0 +1,40 @@
+<?php
+ $title = "Keynote [SIGCSE 2014]";
+ $nav_menu = "keynote";
+ include("../parts/top.php");
+?>
+
+<?php 
+  include("../parts/middle.php"); 
+?>
+
+<div class="row-fluid">
+ <div class="span12" style="text-align: center" id="about"><h2 class="section">SIGCSE 2014 Keynote Speakers</h2></div>
+</div>
+
+    <div class="row-fluid"><h2>Hadi Partovi, co-founder & CEO, <a href="http://code.org/">Code.org</a></h2></div>
+    
+     	<p>
+		Hadi is an entrepreneur and investor, and also co-founder of education non-profit Code.org. As an entrepreneur, he was on the founding teams of Tellme 
+		and iLike. As an angel investor and startup advisor, Hadi’s portfolio includes Facebook, Zappos, Dropbox, OPOWER, Flixster, Bluekai, and many others.
+		</p>
+		<p>
+ 		A graduate of Harvard University, Hadi began his career during the browser wars in the 1990s, when he was Microsoft’s Group Program Manager for Internet 
+ 		Explorer. After the release of IE 5.0, Hadi co-founded Tellme Networks. Tellme was acquired by Microsoft for a reported $800 million. Hadi was General Manager 
+ 		of MSN.com during MSN’s only year of profit, where he delivered 30% annual growth and incubated Start.com (now Live.com).
+ 		</p>
+ 		<p>
+ 		After leaving Microsoft a second time, Hadi co-founded iLike with twin brother Ali Partovi, and together they built the leading music application on the 
+ 		Facebook platform. In 2009, iLike was acquired by MySpace where both Partovis worked as Senior Vice Presidents.
+ 		</p>
+ 		<p> 
+ 		Hadi is a strategic advisor to numerous startups 
+ 		including Facebook, Dropbox, OPOWER, and Bluekai, and serves on the board of TASER International. He is also an active angel investor with a wide range of 
+ 		investments.
+ 		</p>
+
+	
+
+<?php
+ include("../parts/bottom.php");
+?>
---
+++ attendees/presymposium.php
@@ -0,0 +1,82 @@
+<?php
+ $title = "Pre-Symposium Events [SIGCSE 2014]";
+ $nav_menu = "presymposium";
+ include("../parts/top.php");
+?>
+
+<?php 
+  include("../parts/middle.php"); 
+?>
+
+<div class="row-fluid">
+ <div class="span12" style="text-align: center" id="about"><h2 class="section">SIGCSE 2014 Pre-Symposium Events</h2></div>
+</div>
+
+<div class="row-fluid">
+ <div class="span12" style="text-align: center"><h2 class="section">Using Guided Inquiry Activities to Help Students Learn Key Concepts and Process Skills </h2></div>
+ <div class="row-fluid">
+ 	<div class="span12">
+		<p><strong>When:</strong> Wednesday, March 5, 2014, 9:00am to 5:00pm</p>
+		<p><strong>Where:</strong> TBA</p>
+		<p><strong>Cost:</strong> $50</p>
+		<p><strong>Description:</strong> In Process Oriented Guided Inquiry Learning (POGIL), teams of 3-5 learners work on scripted inquiry activities designed to help them discover concepts and construct their own knowledge. Using assigned team roles and meta-cognition, learners develop process skills and individual responsibility. The instructor serves as a facilitator, not a lecturer. Studies show that POGIL can significantly improve student performance.  In this workshop, participants will be introduced to the theory and practice of POGIL. They will experience POGIL activities, learn core practices, and draft parts of activities. This workshop is intended for anyone interested in teaching and learning approaches that are described as active, constructivist, or discovery-based, including both college and high school faculty.
+Please register for this workshop through the project website (POGIL pre-conference event page.)</p>
+
+<p>For additional information please contact Clif Kussmaul or Helen Hu.</p>
+</div>
+</div>
+
+<div class="row-fluid">
+<div class="span12" style="text-align: center"><h2 class="section">Integrating Professional Issues into the Computer Science Curriculum: Teaching Computing Students about the Challenge of Computer Ethics in an Increasingly Automated World Living with Sophisticated Machines </h2></div>
+ <div class="row-fluid">
+ 	<div class="span12">
+<p><strong>When:</strong> Wednesday, March 5, 2014, 1:00pm to 4:30pm </p>
+<p><strong>Where:</strong> TBA</p>
+<p><strong>Cost:</strong> No charge</p>
+<p><strong>Description:</strong> In support of ACM's commitment to professionalism this annual event is designed to help faculty provide students with tools to better understand and resolve their ethical challenges as computing professionals. This year’s event will have a special focus on robots, Google Glass, and other increasingly sophisticated devices.  The workshop will use both lecture and small group activities to introduce and enhance their computer ethics teaching skills. We will present materials to be used in a complete computer ethics course, and that can also be used as examples and exercises in specific technical courses; These materials will include case studies, suggested course syllabi, and suggestions for creating and grading assignments. The workshop will feature demonstrations of several advanced devices, and how they can be used to teach computer ethics issues.
+For additional information and to register for this workshop, please contact Don Getterbarn.</p>
+</div>
+</div>
+
+<div class="row-fluid">
+<div class="span12" style="text-align: center"><h2 class="section">New Educators Workshop</h2></div>
+ <div class="row-fluid">
+ 	<div class="span12">
+<p><strong>When:</strong> Wednesday, March 5, 2014, 9:00am to 5:00pm</p>
+<p><strong>Where:</strong> TBA</p>
+<p><strong>Cost:</strong> No charge</p>
+<p><strong>Description:</strong> Despite the important role teaching plays in many academic careers, the reality is that many new educators receive only minimal teacher training and are often unsure of what career options are available to them. The New Educators Workshop is designed to assist academic-oriented graduate students and pre-tenure faculty in planning and advancing their careers as educators in academia.  Experienced colleagues from a variety of career paths will be there to share their experiences and best practices.   Small-group discussions will enable attendees to focus on the questions or concerns most relevant to their situations, as well as build a support group of like-minded colleagues.   </p>
+<p>The New Educators Workshop is by invitation only after completing and submitting an application. The workshop is free and limited travel support is available for graduate students. </p>
+<p>To find out more and to submit an application, visit <a href="http://dave-reed.com/NEW">http://dave-reed.com/NEW</a> or contact David Reed.   </p>
+</div>
+</div>
+
+<div class="row-fluid">
+<div class="span12" style="text-align: center"><h2 class="section">SIGCAS nowadays: What it is? What should it be?</h2></div>
+ <div class="row-fluid">
+ 	<div class="span12">
+<p><strong>When:</strong> Wednesday, March 5, 2014, 9:00am to 12:00pm</p>
+<p><strong>Where:</strong> TBA</p>
+<p><strong>Cost:</strong> $25</p>
+<p><strong>Description:</strong> This is SIGCAS annual opportunity to meet with members, have a few current presentations, and discuss what's important, and what we should concentrate on.  It will also be a working meeting - after our reports we will divide in small groups that come up with ideas for activities for the coming year.  
+For additional information and registration please contact Netiva Caftori. </p>
+</div>
+</div>
+
+<div class="row-fluid">
+<div class="span12" style="text-align: center"><h2 class="section">SIGCAS nowadays: What it is? What should it be?</h2></div>
+ <div class="row-fluid">
+ 	<div class="span12">
+<p>Git &amp; GitHub Foundations for Educators</p>
+<p><strong>When:</strong> Wednesday, March 5, 2014, 9:00am to 5:00pm</p>
+<p><strong>Where:</strong> Governor's Square 16</p>
+<p><strong>Cost:</strong> No Charge</p>
+<p><strong>Description:</strong> Our foundations class helps you, as a newcomer to Git and GitHub, fully grasp the concepts and application of distributed version control so that you can effectively begin using this productivity-enhancing suite of tools in the classroom. During this live training course, we will explore the foundations of Git and GitHub through practical every-day commands and processes. We will conclude with workflow ideas and practical tips that will help you and your students with assignment submission, grading, feedback, and more.
+Participants must bring a laptop, this will be a hands-on workshop.
+For additional information visit: <a href=" http://github-workshop-sigcse.eventbrite.com/">http://github-workshop-sigcse.eventbrite.com/</a> or contact John Britton or Matthew McCullough</p>
+</div>
+</div>
+
+<?php
+ include("../parts/bottom.php");
+?>
